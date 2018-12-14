be52aec52ec2d7ad911ca7a18745dc59e732ead2
laughingfalcon <ac_au_a@hotmail.com>
2018-12-14 20:30:09 -0200

    alteração no xml principal, alteração para melhorar ('refatorar') os nomes dos métodos

--- AppGPS/MainActivity.cs
+++ AppGPS/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
-using System;
 using Plugin.Geolocator;
+using System;
 
 namespace AppGPS
 {
@@ -15,27 +14,27 @@ namespace AppGPS
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            FindViewById<Button>(Resource.Id.testarGps).Click += TestarGpsUsavel;
+            FindViewById<Button>(Resource.Id.obterPosicao).Click += BuscarDadosPosicao;
         }
 
 
 
-        private void botaoTestes(object enviador, EventArgs evento) {
-            if (!CrossGeolocator.Current.IsGeolocationAvailable) {
-                Toast.MakeText(this, 'NaN Pos', ToastLength.Long).Show();
-            }
-            if (!CrossGeolocator.IsSupported){
-                Toast.MakeText(this, 'API insurportável', ToastLength.Long).Show();
-            }
-
+        private void TestarGpsUsavel(object enviador, EventArgs evento) {
+            //if (!CrossGeolocator.Current.IsGeolocationAvailable) {
+            //    Toast.MakeText(this, 'NaN Pos', ToastLength.Long).Show();
+            //}
+            //if (!CrossGeolocator.IsSupported){
+            //    Toast.MakeText(this, 'API insurportável', ToastLength.Long).Show();
+            //}
         }
-        private async void botaoPosicao(object enviador, EventArgs evento) {
+        private async void BuscarDadosPosicao(object enviador, EventArgs evento) {
             var localizacao = CrossGeolocator.Current;
             localizacao.DesiredAccuracy = 50;
 
             var posicao = await localizacao.GetPositionAsync(TimeSpan.FromSeconds(10));
-
-            lblLatitude.Text = posicao.Latitude.ToString();
-            lblLongitude.Text = posicao.Longitude.ToString();
+            
         }
     }
 }
